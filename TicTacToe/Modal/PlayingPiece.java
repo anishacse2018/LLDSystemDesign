5dccb8b95e208ec6f862b185aefa31494bf3d4ab
Anisha Kollipara <65843313+anishacse2018@users.noreply.github.com>
2023-08-15 11:12:02 +0000

    TicTacToe

--- TicTacToe/Modal/PlayingPiece.java
+++
@@ -1,7 +0,0 @@
-public class PlayingPiece{
-    private PieceType piecetype;
-    PlayingPiece(PieceType piecetype){
-       this.piecetype= piecetype;
-       System.out.println(this.piecetype);
-    }
-}
