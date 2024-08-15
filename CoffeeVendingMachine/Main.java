95d0c6629b047825b0db07c4fef56ccff86b072c
Anisha Kollipara <65843313+anishacse2018@users.noreply.github.com>
2024-08-15 16:51:08 +0530

    Create Main.java

---
+++ CoffeeVendingMachine/Main.java
@@ -0,0 +1,9 @@
+import model.CoffeeVendingMachine;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Coffee Vending Machine!");
+        CoffeeVendingMachine.run();
+    }
+}
