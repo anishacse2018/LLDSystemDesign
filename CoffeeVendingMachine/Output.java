c973604ca337b2b9612cf68a640eca99fe7dd5fe
Anisha Kollipara <65843313+anishacse2018@users.noreply.github.com>
2024-08-15 16:53:55 +0530

    Create Output.java

---
+++ CoffeeVendingMachine/Output.java
@@ -0,0 +1,10 @@
+anisha.k@HMDVANISHAK src % javac -source 17 -target 17 Main.java
+anisha.k@HMDVANISHAK src % java Main.java                       
+Welcome to Coffee Vending Machine!
+Coffee: Expresso  Price: 40.0
+Coffee: Latte  Price: 50.0
+Coffee: capaccino  Price: 30.0
+Dispensing Coffee: Expresso
+Insufficient Payment done for coffee: Latte
+Please find ur change: 60.0
+Dispensing Coffee: capaccino
