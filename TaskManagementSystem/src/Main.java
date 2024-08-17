7b111a11ece55a1d792706220f4229f91b1a354f
Anisha Kollipara <65843313+anishacse2018@users.noreply.github.com>
2024-08-17 21:38:04 +0530

    Create Main.java

---
+++ TaskManagementSystem/src/Main.java
@@ -0,0 +1,8 @@
+import model.TaskManagerDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Task Manager!!");
+        TaskManagerDemo.run();
+    }
+}
