f9c0c038b49921c9c7398c804df812cbac98a596
Anisha Kollipara <65843313+anishacse2018@users.noreply.github.com>
2024-08-24 11:04:04 +0530

    Create Road.java

---
+++ TrafficSignalControlSystem/src/model/Road.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Road {
+
+    private String name;
+    private String id;
+    private TrafficLight trafficLight;
+
+    public Road(String name,String id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public TrafficLight getTrafficLight() {
+        return trafficLight;
+    }
+
+    public void setTrafficLight(TrafficLight trafficLight) {
+        this.trafficLight = trafficLight;
+    }
+}
